fb7c59cde266dc6ea0023a772d9b600ed6e34e8e
NeXTSTEP <leiggee@126.com>
2016-01-04 17:54:07 +0800

    自定义刷新控件

---
+++ Refresh-Simple/Source/HMRefreshControl.h
@@ -0,0 +1,13 @@
+//
+//  HMRefreshControl.h
+//  Refresh-Simple
+//
+//  Created by NeXTSTEP on 16/1/4.
+//  Copyright © 2016年 NeXTSTEP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMRefreshControl : UIControl
+- (void) startAnimtion;
+@end
---
+++ Refresh-Simple/Source/HMRefreshControl.m
@@ -0,0 +1,17 @@
+//
+//  HMRefreshControl.m
+//  Refresh-Simple
+//
+//  Created by NeXTSTEP on 16/1/4.
+//  Copyright © 2016年 NeXTSTEP. All rights reserved.
+//
+
+#import "HMRefreshControl.h"
+
+@implementation HMRefreshControl
+
+- (void)startAnimtion {
+    NSLog(@"开始刷新");
+}
+
+@end
